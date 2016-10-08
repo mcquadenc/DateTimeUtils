1ae8a86d8ee793a1b004156afe3212617d29f752
mcquadenc <mcquadenc@gmail.com>
2016-10-08 01:50:52 -0300

    correcao do retorno de: int para Date

--- date.py
+++ date.py
@@ -8,10 +8,11 @@ class DateUtils(object):
     U'''
         Recupera todos os domingos de um determinado mês e ano em formto "Date" <datetime>
     '''
+
     def get_month_sundays(self, year, month):
         list_sunday_date = []
         list_sunday = [week[-1] for week in calendar.monthcalendar(year, month) if week[-1] > 0]
         for sunday in list_sunday:
             sunday_date = datetime(year=year, month=month, day=sunday).date()
             list_sunday_date.append(sunday_date)
-        return list_sunday
+        return list_sunday_date
